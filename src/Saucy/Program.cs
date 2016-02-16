f8ce07c1e5fd95bd90077c2de1493576949ed586
acraven <andrew@epworth-consulting.co.uk>
2016-02-16 08:04:48 +0000

    Add solution

---
+++ src/Saucy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saucy
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("USAGE:\n   Saucy --help | --restore");
+      }
+   }
+}
