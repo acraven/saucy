2f10756ed1b6b11baaaaebbfd97220bd1e86ee74
acraven <andrew@epworth-consulting.co.uk>
2016-02-26 16:55:31 +0000

    Add file caught by ignore rules

---
+++ src/Saucy/SaucySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Saucy
+{
+   public class SaucySettings
+   {
+      public string PackagesFolder { get; set; } = ".saucy";
+
+      public string ConfigFile { get; set; } = "saucy.json";
+   }
+}
