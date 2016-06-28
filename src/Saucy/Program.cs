5aaf784652a2e95a72cee5b33beccbf3b0626f7f
acraven <andrew@epworth-consulting.co.uk>
2016-06-28 12:00:19 +0100

    Add exception handler to main()

--- src/Saucy/Program.cs
+++ src/Saucy/Program.cs
@@ -4,27 +4,40 @@ using Saucy.Providers.GitHub;
 
 namespace Saucy
 {
+   using System;
+
    public class Program
    {
       public static int Main(string[] args)
       {
-         var settings = new SaucySettings();
+         var consoleWriter = new ConsoleWriter();
 
-         ILogger logger = new VerboseLogger();
+         try
+         {
+            var settings = new SaucySettings();
 
-         var restoreVerb = new SaucyCommandLine(
-            new PackagesRestorer(
-               new JsonLoader(),
-               new ProviderMatcher(new GitHubProvider(new FolderSync(logger))),
-               new ConsoleWriter(),
-               settings),
-            settings);
+            ILogger logger = new VerboseLogger();
 
-         var runner = new Runner();
-         runner.Register(restoreVerb);
+            var restoreVerb = new SaucyCommandLine(
+               new PackagesRestorer(
+                  new JsonLoader(),
+                  new ProviderMatcher(new GitHubProvider(new FolderSync(logger))),
+                  consoleWriter,
+                  settings),
+               settings);
 
-         var exitCode = runner.Run(args);
-         return exitCode;
+            var runner = new Runner();
+            runner.Register(restoreVerb);
+
+            var exitCode = runner.Run(args);
+            return exitCode;
+         }
+         catch (Exception e)
+         {
+            consoleWriter.Write(e.Message);
+            consoleWriter.Write(e.StackTrace);
+            return -1;
+         }
       }
    }
 }
